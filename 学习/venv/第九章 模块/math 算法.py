9773de6f3b2283c9c7bd9657e115421df0d71ee3
ZhouBaole <1542530095@qq.com>
2020-06-15 12:28:09 +0800

    加了算法

---
+++ 学习/venv/第九章 模块/math 算法.py
@@ -0,0 +1,15 @@
+import math
+"""
+两个常量
+"""
+print("圆周率（math.pi）：",math.pi)
+print("自然常数(math.e)：",math.e)
+"""
+运算函数
+"""
+#向上取整
+print("向上取整（1.7）:",math.ceil(1.7))
+#向下取整
+print("向下取整（1.7）:",math.floor(1.7))
+#指数运算
+print("指数运算（3^3）:",math.pow(3,3))
